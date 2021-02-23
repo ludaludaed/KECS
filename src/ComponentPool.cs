5f0af82e70fe4dfbe71fc5fc925b5ebf89209819
_ludaludaed <43884394+ludaludaed@users.noreply.github.com>
2021-02-23 19:33:49 +0300

    Add files via upload

---
+++ src/ComponentPool.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace KECS
+{
+    internal static class EcsTypeManager
+    {
+        internal static int ComponentTypesCount = 0;
+    }
+    
+    internal static class ComponentTypeInfo<T> where T : struct
+    {
+        internal static readonly int TypeIndex;
+        internal static readonly Type Type;
+        
+        private static object _lockObject = new object();
+
+        static ComponentTypeInfo()
+        {
+            lock (_lockObject)
+            {
+                TypeIndex = EcsTypeManager.ComponentTypesCount++;
+                Type = typeof(T);
+            }
+        }
+    }
+    
+    internal interface IComponentPool : IDisposable
+    {
+        public void Remove(int entityId);
+        public void EnsureLength(int capacity);
+    }
+
+
+    internal sealed class ComponentPool<T> : IComponentPool where T : struct
+    {
+        private SparseSet<T> _components;
+        private int Length => _components.Count;
+        private T _empty;
+        private World _owner;
+
+        public ref T Empty() => ref _empty;
+
+        public ComponentPool(World world)
+        {
+            _owner = world;
+            _components = new SparseSet<T>(world.Config.ComponentsCapacity, world.Config.ComponentsCapacity);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ref T Get(int entityId)
+        {
+            return ref _components.GetValue(entityId);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Add(int entityId, T value)
+        {
+            _components.Add(entityId, value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Remove(int entityId)
+        {
+            _components.Remove(entityId);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Set(int entityId, T value)
+        {
+            _components.Set(entityId, value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void EnsureLength(int capacity)
+        {
+            _components.EnsureSparseCapacity(capacity);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Dispose()
+        {
+            _components.Clear();
+            _components = null;
+        }
+    }
+}
---
+++ src/Systems.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KECS
+{
+    public interface IRunSystem
+    {
+        void Run(float deltaTime);
+    }
+
+    public abstract class SystemBase : IDisposable
+    {
+        protected World World;
+        protected Systems MySystemsGroup;
+        public abstract void Initialize();
+
+        internal void StartUp(World world, Systems systems)
+        {
+            World = world;
+            MySystemsGroup = systems;
+            OnLaunch();
+        }
+
+        public virtual void OnLaunch()
+        {
+        }
+
+        public virtual void OnDestroy()
+        {
+        }
+
+        public virtual void PostDestroy()
+        {
+        }
+
+        public void Dispose() => OnDestroy();
+    }
+
+    internal sealed class SystemData
+    {
+        public bool IsEnable;
+        public SystemBase Base;
+        public IRunSystem runSystemImpl;
+    }
+
+    public sealed class Systems : IDisposable
+    {
+        private readonly Dictionary<int, SystemData> _systems;
+
+        private readonly List<SystemData> _runSystems;
+        private readonly List<SystemData> _allSystems;
+
+        private readonly World _world;
+        private bool _initialized;
+        private bool _destroyed;
+
+        public string Name { get; private set; }
+
+        public Systems(World world, string name = "DEFAULT_SYSTEMS")
+        {
+            Name = name;
+            _world = world;
+            _initialized = false;
+            _destroyed = false;
+            _systems = new Dictionary<int, SystemData>();
+            _allSystems = new List<SystemData>();
+            _runSystems = new List<SystemData>();
+        }
+
+        public Systems Add<T>() where T : SystemBase, new()
+        {
+            var obj = new T();
+            return Add(obj);
+        }
+
+        private Systems Add<T>(T systemValue) where T : SystemBase
+        {
+            if (_initialized)
+            {
+                throw new Exception("|KECS| System cannot be added after initialization.");
+            }
+
+            int hash = typeof(T).GetHashCode();
+
+            if (!_systems.ContainsKey(hash))
+            {
+                var systemData = new SystemData {IsEnable = true, Base = systemValue};
+                _allSystems.Add(systemData);
+                systemValue.StartUp(_world, this);
+
+                if (systemValue is IRunSystem system)
+                {
+                    systemData.runSystemImpl = system;
+                    _runSystems.Add(systemData);
+                    _systems.Add(hash, systemData);
+                }
+            }
+
+            return this;
+        }
+
+        public Systems Disable<T>() where T : SystemBase
+        {
+            int hash = typeof(T).GetHashCode();
+
+            if (_systems.TryGetValue(hash, out var systemValue))
+            {
+                systemValue.IsEnable = false;
+            }
+
+            return this;
+        }
+
+        public Systems Enable<T>() where T : SystemBase
+        {
+            int hash = typeof(T).GetHashCode();
+
+            if (_systems.TryGetValue(hash, out var systemValue))
+            {
+                systemValue.IsEnable = true;
+            }
+
+            return this;
+        }
+
+        public Systems OneFrame<T>() where T : struct
+        {
+            return Add(new RemoveOneFrame<T>());
+        }
+
+        public void Run(float deltaTime)
+        {
+            if (!_initialized)
+            {
+                throw new Exception("|KECS| Systems haven't initialized yet.");
+            }
+
+            if (_destroyed)
+            {
+                throw new Exception("|KECS| The systems were destroyed. You cannot update them.");
+            }
+
+            foreach (var update in _runSystems)
+            {
+                if (update.IsEnable)
+                {
+                    update.runSystemImpl?.Run(deltaTime);
+                }
+            }
+        }
+
+        public void Initialize()
+        {
+            if (_destroyed)
+            {
+                throw new Exception("|KECS| The systems were destroyed. You cannot initialize them.");
+            }
+
+            _initialized = true;
+            foreach (var initializer in _allSystems)
+            {
+                initializer.Base.Initialize();
+            }
+        }
+
+        public void Destroy()
+        {
+            if (_destroyed)
+            {
+                throw new Exception("|KECS| The systems were destroyed. You cannot destroy them.");
+            }
+
+            _destroyed = true;
+
+            foreach (var destroy in _allSystems)
+            {
+                if (destroy.IsEnable)
+                {
+                    destroy.Base.OnDestroy();
+                }
+            }
+
+            foreach (var postDestroy in _allSystems)
+            {
+                if (postDestroy.IsEnable)
+                {
+                    postDestroy.Base.PostDestroy();
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            _systems.Clear();
+            _allSystems.Clear();
+            _runSystems.Clear();
+        }
+    }
+
+    internal class RemoveOneFrame<T> : SystemBase, IRunSystem where T : struct
+    {
+        private Filter _filter;
+
+        public override void Initialize()
+        {
+            _filter = World.Filter.With<T>();
+        }
+
+        public void Run(float deltaTime)
+        {
+            foreach (var ent in _filter)
+            {
+                ent.RemoveComponent<T>();
+            }
+        }
+    }
+}
