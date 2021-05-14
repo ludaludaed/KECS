fbba69c8f3bbfdf1eae17a269dd1b35f13c1883f
_ludaludaed <43884394+ludaludaed@users.noreply.github.com>
2021-05-14 21:17:42 +0300

    Add files via upload

--- KECS.cs
+++ KECS.cs
@@ -2402,7 +2402,10 @@ namespace Ludaludaed.KECS
     // BIT MASK
     //=============================================================================
 
-
+#if ENABLE_IL2CPP
+    [Unity.IL2CPP.CompilerServices.Il2CppSetOption (Unity.IL2CPP.CompilerServices.Option.NullChecks, false)]
+    [Unity.IL2CPP.CompilerServices.Il2CppSetOption (Unity.IL2CPP.CompilerServices.Option.ArrayBoundsChecks, false)]
+#endif
     public struct BitMask
     {
         private const int CHUNK_CAPACITY = sizeof(ulong) * 8;
