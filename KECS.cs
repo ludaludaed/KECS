4a60d0d8d403a0c82f26d3c9e19a0809e63c3159
_ludaludaed <43884394+ludaludaed@users.noreply.github.com>
2021-12-26 17:36:27 +0300

    Add files via upload

--- KECS.cs
+++ KECS.cs
@@ -2313,23 +2313,19 @@ namespace Ludaludaed.KECS {
 }
 
 #if ENABLE_IL2CPP
-namespace Unity.IL2CPP.CompilerServices
-{
-    enum Option
-    {
+namespace Unity.IL2CPP.CompilerServices {
+    enum Option {
         NullChecks = 1,
         ArrayBoundsChecks = 2
     }
 
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property, Inherited =
         false, AllowMultiple = true)]
-    class Il2CppSetOptionAttribute : Attribute
-    {
+    class Il2CppSetOptionAttribute : Attribute {
         public Option Option { get; private set; }
         public object Value { get; private set; }
 
-        public Il2CppSetOptionAttribute(Option option, object value)
-        {
+        public Il2CppSetOptionAttribute(Option option, object value) {
             Option = option;
             Value = value;
         }
