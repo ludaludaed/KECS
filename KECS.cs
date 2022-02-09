77e4782eee7721e2b8c0a0ac49413a33c962bb03
_ludaludaed <43884394+ludaludaed@users.noreply.github.com>
2022-02-09 17:20:16 +0300

    Add files via upload

--- KECS.cs
+++ KECS.cs
@@ -193,7 +193,7 @@ namespace Ludaludaed.KECS {
             _archetypeSignatures.Set(emptyArch.Hash, emptyArch);
             Archetypes.Add(emptyArch);
             _isAlive = true;
-            Config = config;
+            this.Config = config;
             Name = name;
         }
 
